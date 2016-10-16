dcc155da713386aa6101927879a3d6e4d5a20bba
KhaledaNYC <khaleda.khan87@gmail.com>
2016-10-16 16:14:51 -0400

    even fib sum classes

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+
+  start = 0
+  num1 = 0
+  num2 = 1
+  sum_fib = []
+  total = 0
+  while total < limit
+    total = num1 + num2
+    num1 = num2
+    num2 = total
+    sum_fib << total
+    start += 1
+  end
+  sum_fib.select{|num|(num < limit && num%2==0)}.reduce(:+)
+
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,24 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+  attr_accessor :limit, :sum
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def sum
+    start = 0
+    num1 = 0
+    num2 = 1
+    sum_fib = []
+    total = 0
+    while total < self.limit
+      total = num1 + num2
+      num1 = num2
+      num2 = total
+      sum_fib << total
+      start += 1
+    end
+    sum_fib.select{|num|(num < self.limit && num%2==0)}.reduce(:+)
+  end
+end
